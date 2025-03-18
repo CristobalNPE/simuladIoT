e041b1ddd575c2a02b229ff2c0dc6c1e40dd99ae
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-03-18 18:52:56 -0300

    add github link

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,17 +8,19 @@ import {StatusCard} from "~/components/status-card";
 import {MessageHistoryCard} from "~/components/message-history-card";
 import type {DeviceInfo, DeviceInfoWithTimestamp, DeviceType} from "~/types/device.types";
 import type {ConnectionConfig} from "~/types/connection.types";
+import {ExternalLink, Github} from "lucide-react";
+import {Tooltip, TooltipContent, TooltipTrigger} from "~/components/ui/tooltip";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        {title: "Simulador Dispositivos IOT"},
+        {title: "SimulaDIoT - Simulador Dispositivos de IOT"},
         {name: "description", content: "Simula dispositivos IOT y envía datos a tu API"},
     ];
 }
 
 export default function Home() {
 
-    const {connectionConfig, getRestEndpoint} = useConnection();
+    const {connectionConfig} = useConnection();
 
     const [lastResponse, setLastResponse] = useState<{ status: number; message: string } | null>(null)
     const [messageHistory, setMessageHistory] = useState<DeviceInfoWithTimestamp[]>([])
@@ -61,13 +63,17 @@ export default function Home() {
             <header className={"flex items-center justify-between p-4 border-b border-border shadow-xs"}>
                 <div className={"container mx-auto flex items-center gap-4 justify-between"}>
                     <div>
-                        <h1 className={"text-lg font-semibold"}>Simulador Dispositivos IOT</h1>
+                        <h1 className={"text-lg font-semibold"}>SimulaDIoT - Simulador de Dispositivos IOT</h1>
                         <p className={"text-xs text-muted-foreground "}>
                             Prueba tu API con dispositivos ESP32 y Zigbee simulados. Configura y envía datos a tu
                             servidor.
                         </p>
                     </div>
-                    <ThemeSwitch/>
+                    <div className={"flex items-center gap-4"}>
+                        <GithubLink/>
+                        <ThemeSwitch/>
+                    </div>
+
                 </div>
             </header>
 
@@ -101,3 +107,24 @@ export default function Home() {
     );
 }
 
+function GithubLink() {
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild>
+
+                <div className={" h-8 w-fit flex justify-center items-center p-2 rounded-full border"}>
+                    <a
+                        href="https://github.com/CristobalNPE/iot-device-simulator" target="_blank"
+                        rel="noreferrer">
+                        <Github size={20}/>
+                    </a>
+                </div>
+
+
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>Ver en GitHub <ExternalLink size={14} className={"inline-flex ml-1"}/></p>
+            </TooltipContent>
+        </Tooltip>
+    )
+}
