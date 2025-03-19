5116588aa59e17a047202dd063561b05b16c59f4
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-03-18 22:37:14 -0300

    add disabled prop and status

--- app/components/payload-tab.tsx
+++ app/components/payload-tab.tsx
@@ -3,20 +3,22 @@ import { Textarea } from "~/components/ui/textarea"
 import {formatJSONMessage} from "~/utils/format.utils";
 
 interface PayloadTabProps {
+    disabled: boolean
     deviceId: string
     customPayload: string
     setCustomPayload: (value: string) => void
 }
 
-export function PayloadTab({ deviceId, customPayload, setCustomPayload }: PayloadTabProps) {
+export function PayloadTab({ disabled,deviceId, customPayload, setCustomPayload }: PayloadTabProps) {
     return (
         <div className="space-y-2">
             <Label htmlFor={`payload-${deviceId}`}>JSON Payload</Label>
             <Textarea
+                disabled={disabled}
                 id={`payload-${deviceId}`}
                 value={formatJSONMessage(customPayload)}
                 onChange={(e) => setCustomPayload(e.target.value)}
-                className="font-mono text-sm h-[200px]"
+                className="font-mono text-sm h-[200px] "
             />
         </div>
     )
