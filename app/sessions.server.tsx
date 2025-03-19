571c63987e01ce9d0d8d69836c5d7e6cef25b45f
Cristobal Pulgar E <cristobal.npe@gmail.com>
2025-03-18 23:50:14 -0300

    add redirects and favicon

--- app/sessions.server.tsx
+++ app/sessions.server.tsx
@@ -1,5 +1,13 @@
-import { createThemeSessionResolver } from "remix-themes";
-import { createCookieSessionStorage } from "react-router";
+import {createThemeSessionResolver} from "remix-themes";
+import {createCookieSessionStorage} from "react-router";
+import dotenv from "dotenv";
+
+//TODO: move env config someday 🎄
+
+dotenv.config();
+if (!process.env.SESSION_SECRET) {
+    throw new Error("SESSION_SECRET must be defined in environment variables");
+}
 
 const sessionStorage = createCookieSessionStorage({
     cookie: {
@@ -7,7 +15,7 @@ const sessionStorage = createCookieSessionStorage({
         path: "/",
         httpOnly: true,
         sameSite: "lax",
-        secrets: process.env.VITE_SESSION_SECRET.split(','),
+        secrets: process.env.SESSION_SECRET.split(','),
         secure: process.env.NODE_ENV === 'production',
     },
 });
